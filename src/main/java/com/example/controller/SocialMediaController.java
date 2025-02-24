9bbf9cce81d8544521f2d4e3548c832e4c0854f5
Anthony Siragusa <ajsirag@gmail.com>
2025-02-24 10:06:50 +0000

    Created Registration Login createMessage Functions

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,23 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +25,61 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@Controller
 public class SocialMediaController {
+
+    private AccountService accountService;
+    private MessageService messageService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService)
+    {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<Account> register(@RequestBody Account account){
+        Account optionalAccount = accountService.findByUsername(account.getUsername());
+        if(optionalAccount != null)
+        {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        }
+        else if(account.getUsername().isEmpty() || account.getPassword().length() < 4)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        Account createdAccount;
+        createdAccount = accountService.register(account);
+        return ResponseEntity.status(HttpStatus.OK).body(createdAccount);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<Account> login(@RequestBody Account account){
+        Account optionalAccount;
+        optionalAccount = accountService.login(account);
+        if(optionalAccount == null)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.OK).body(optionalAccount);
+        }
+    }
+
+    @PostMapping("messages")
+    public ResponseEntity<Message> createMessage(@RequestBody Message message){
+        Account optionalAccount;
+        optionalAccount = accountService.findById(message.getPostedBy());
+        if(optionalAccount != null && !message.getMessageText().isEmpty() && message.getMessageText().length() <= 255)
+        {
+            Message createdMessage;
+            createdMessage = messageService.createMessage(message);
+            return ResponseEntity.status(HttpStatus.OK).body(createdMessage);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,57 @@
 package com.example.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import java.util.Optional;
 
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account register(Account account)
+    {
+        return accountRepository.save(account);
+    }
+
+    public Account findByUsername(String username){
+        Optional<Account> account = accountRepository.findByUsername(username);
+        if(account.isPresent()){
+            return account.get();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Account findById(Integer id)
+    {
+        Optional<Account> account = accountRepository.findById(id);
+        if(account.isPresent()){
+            return account.get();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Account login(Account account)
+    {
+        Optional<Account> optionalAccount;
+        optionalAccount = accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
+        if(optionalAccount.isPresent())
+        {
+            return optionalAccount.get();
+        }
+        return null;
+    }
+
 }
